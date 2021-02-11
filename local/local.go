ddcbabf4511460fe570a4bbb5a110318679467dd
Sergey Fedyushin <SerFed13@gmail.com>
2021-02-11 14:12:03 +0600

    BAC-152 Add local storage

---
+++ local/local.go
@@ -0,0 +1,144 @@
+package local
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type (
+	Config struct {
+		StorageKey string
+		Endpoint   string
+		Root       string
+		BufferSize int //bytes
+	}
+
+	Local struct {
+		cfg Config
+	}
+)
+
+var (
+	defaultStorageKey = "file"
+	defaultEndpoint   = "http://localhost:8080/"
+	defaultRoot       = ""
+	defaultBufferSize = 16 * 1024
+)
+
+var ErrMethodNotImplemented = errors.New("Method is not implemented")
+
+func New(cfg *Config) *Local {
+	if cfg == nil {
+		cfg = &Config{
+			StorageKey: defaultStorageKey,
+			Endpoint:   defaultEndpoint,
+			Root:       defaultRoot,
+			BufferSize: defaultBufferSize,
+		}
+	}
+
+	return &Local{
+		cfg: *cfg,
+	}
+}
+
+func (b *Local) Store(filePath, path string) (cLink string, err error) {
+	err = copy(filePath, b.cfg.Root+strings.TrimLeft(path, "/"), b.cfg.BufferSize)
+	if err != nil {
+		return "", err
+	}
+
+	cLink = b.pathToCLink(path)
+	return cLink, nil
+}
+
+func (b *Local) GetURL(cLink string, options ...interface{}) (URL string) {
+	if !checkStorageKey(cLink, b.cfg.StorageKey) {
+		log.Println("Failed check storage key:", cLink, b.cfg.StorageKey)
+		return ""
+	}
+	u, err := url.Parse(endSlash(b.cfg.Endpoint) + strings.TrimLeft(cLink, b.cfg.StorageKey+":"))
+	if err != nil {
+		log.Println("Parse err:", err)
+		return ""
+	}
+	return u.String()
+}
+
+func (b *Local) Remove(cLink string) (err error) {
+	path := b.cLinkToPath(cLink)
+	err = os.Remove(path)
+
+	return err
+}
+
+func copy(src, dst string, bufferSize int) error {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file.", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	err = os.MkdirAll(filepath.Dir(dst), 0770)
+	if err != nil {
+		return err
+	}
+
+	os.Remove(dst)
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	buf := make([]byte, bufferSize)
+	for {
+		n, err := source.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+
+		if _, err := destination.Write(buf[:n]); err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (b *Local) pathToCLink(path string) (cLink string) {
+	return fmt.Sprintf("%s:%s", b.cfg.StorageKey, strings.TrimLeft(path, "/"))
+}
+
+func (b *Local) cLinkToPath(cLink string) (path string) {
+	if !checkStorageKey(cLink, b.cfg.StorageKey) {
+		return ""
+	}
+	return endSlash(b.cfg.Root) + strings.TrimLeft(cLink, b.cfg.StorageKey+":")
+}
+
+func checkStorageKey(cLink string, storageKey string) (ok bool) {
+	return strings.Contains(cLink, storageKey+":")
+}
+
+func endSlash(s string) string {
+	return strings.TrimRight(s, "/") + "/"
+}
---
+++ local/local_test.go
@@ -0,0 +1,200 @@
+package local
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+)
+
+var (
+	testEndpoint            = "http://url.com/files"
+	testStorageKey          = "localFile"
+	testRoot                = "test/"
+	testRootWithoutEndSlash = strings.TrimRight(testRoot, "/")
+
+	testCfg = &Config{
+		StorageKey: testStorageKey,
+		Endpoint:   testEndpoint,
+		Root:       testRoot,
+		BufferSize: 32 * 1024,
+	}
+
+	testStorage = New(testCfg)
+)
+
+func TestEndSlash(t *testing.T) {
+	var flagtests = []struct {
+		in  string
+		out string
+	}{
+		{"/folder/file1.jpg", "/folder/file1.jpg/"},
+		{"/file1.jpg", "/file1.jpg/"},
+		{"http://ya.ru", "http://ya.ru/"},
+		{"http://ya.ru/", "http://ya.ru/"},
+		{"http://ya.ru/page", "http://ya.ru/page/"},
+		{"http://ya.ru/page/", "http://ya.ru/page/"},
+		{"file1", "file1/"},
+		{"file1/", "file1/"},
+		{"file1//", "file1/"},
+		{"file1///", "file1/"},
+	}
+
+	for _, tt := range flagtests {
+		t.Run(tt.in, func(t *testing.T) {
+			s := endSlash(tt.in)
+			if s == tt.out {
+				t.Logf("got %q, want %q", s, tt.out)
+			} else {
+				t.Errorf("got %q, want %q", s, tt.out)
+			}
+		})
+	}
+}
+
+func TestPathToCLink(t *testing.T) {
+	var flagtests = []struct {
+		in  string
+		out string
+	}{
+		{"/folder/file1.jpg", testStorageKey + ":" + "folder/file1.jpg"},
+		{"/folder/folder2/file2.jpg", testStorageKey + ":" + "folder/folder2/file2.jpg"},
+		{"folder/file1.jpg", testStorageKey + ":" + "folder/file1.jpg"},
+		{"folder/folder2/file2.jpg", testStorageKey + ":" + "folder/folder2/file2.jpg"},
+		{"/file1.jpg", testStorageKey + ":" + "file1.jpg"},
+		{"/file 1.jpg", testStorageKey + ":" + "file 1.jpg"},
+		{"file1.jpg", testStorageKey + ":" + "file1.jpg"},
+	}
+
+	for _, tt := range flagtests {
+		t.Run(tt.in, func(t *testing.T) {
+			s := testStorage.pathToCLink(tt.in)
+			if s == tt.out {
+				t.Logf("got %q, want %q", s, tt.out)
+			} else {
+				t.Errorf("got %q, want %q", s, tt.out)
+			}
+		})
+	}
+}
+
+func TestCLinkToUrl(t *testing.T) {
+	var flagtests = []struct {
+		in  string
+		out string
+	}{
+		{"localFile:folder/file1.jpg", testRootWithoutEndSlash + "/folder/file1.jpg"},
+		{"localFile:folder/folder2/file2.jpg", testRootWithoutEndSlash + "/folder/folder2/file2.jpg"},
+		{"localFile:file1.jpg", testRootWithoutEndSlash + "/file1.jpg"},
+		{"localFile:file 1.jpg", testRootWithoutEndSlash + "/file 1.jpg"},
+		{"anotherKey:file1.jpg", ""},
+	}
+
+	for _, tt := range flagtests {
+		t.Run(tt.in, func(t *testing.T) {
+			s := testStorage.cLinkToPath(tt.in)
+			if s == tt.out {
+				t.Logf("got %q, want %q", s, tt.out)
+			} else {
+				t.Errorf("got %q, want %q", s, tt.out)
+			}
+		})
+	}
+}
+
+func TestGetUrl(t *testing.T) {
+	var flagtests = []struct {
+		in  string
+		out string
+	}{
+		{testStorageKey + ":" + "folder/file1.jpg", endSlash(testEndpoint) + "folder/file1.jpg"},
+		{testStorageKey + ":" + "folder/folder2/file2.jpg", endSlash(testEndpoint) + "folder/folder2/file2.jpg"},
+		{testStorageKey + ":" + "folder/file1.jpg", endSlash(testEndpoint) + "folder/file1.jpg"},
+		{testStorageKey + ":" + "folder/folder2/file2.jpg", endSlash(testEndpoint) + "folder/folder2/file2.jpg"},
+		{testStorageKey + ":" + "file1.jpg", endSlash(testEndpoint) + "file1.jpg"},
+		{testStorageKey + ":" + "file 1.jpg", endSlash(testEndpoint) + "file%201.jpg"},
+	}
+
+	for _, tt := range flagtests {
+		t.Run(tt.in, func(t *testing.T) {
+			s := testStorage.GetURL(tt.in)
+			if s == tt.out {
+				t.Logf("got %q, want %q", s, tt.out)
+			} else {
+				t.Errorf("got %q, want %q", s, tt.out)
+			}
+		})
+	}
+}
+
+func TestStore(t *testing.T) {
+	var flagtests = []struct {
+		in  string
+		out string
+	}{
+		{"/folder/file1.jpg", testStorageKey + ":" + "folder/file1.jpg"},
+		{"/folder/folder2/file2.jpg", testStorageKey + ":" + "folder/folder2/file2.jpg"},
+		{"folder/file1.jpg", testStorageKey + ":" + "folder/file1.jpg"},
+		{"folder/folder2/file2.jpg", testStorageKey + ":" + "folder/folder2/file2.jpg"},
+		{"/file1.jpg", testStorageKey + ":" + "file1.jpg"},
+		{"/file 1.jpg", testStorageKey + ":" + "file 1.jpg"},
+		{"file1.jpg", testStorageKey + ":" + "file1.jpg"},
+	}
+
+	tmp := "file1"
+	ioutil.WriteFile(tmp, []byte("hello\ngo\n"), 0644)
+
+	for _, tt := range flagtests {
+		t.Run(tt.in, func(t *testing.T) {
+			cLink, err := testStorage.Store(tmp, tt.in)
+			if err != nil {
+				t.Errorf("Store err: %q", err)
+			}
+			if cLink != tt.out {
+				t.Errorf("got %q, want %q", cLink, tt.out)
+			} else {
+				t.Logf("got %q, want %q", cLink, tt.out)
+			}
+
+			fullPath := testStorage.cLinkToPath(cLink)
+			fileInfo, err := os.Stat(fullPath)
+			if err != nil {
+				t.Errorf("Check file err: %q", err)
+			}
+			tmpFileInfo, _ := os.Stat(tmp)
+			if fileInfo.Size() != tmpFileInfo.Size() {
+				t.Errorf("Not equal size: %q", cLink)
+			}
+		})
+	}
+
+	//clear files
+	os.Remove(tmp)
+	os.RemoveAll(testRoot)
+}
+
+func TestRemove(t *testing.T) {
+	tmp := "file1"
+	ioutil.WriteFile(tmp, []byte("hello\ngo\n"), 0644)
+	cLink, _ := testStorage.Store(tmp, "/r_test/file.txt")
+
+	fullPath := testStorage.cLinkToPath(cLink)
+	_, err := os.Stat(fullPath)
+	if err != nil {
+		t.Errorf("Check file err: %q", err)
+	}
+
+	err = testStorage.Remove(cLink)
+	if err != nil {
+		t.Errorf("Remove err: %q", err)
+	}
+
+	_, err = os.Stat(fullPath)
+	if err == nil {
+		t.Errorf("Check removed file err: %q", err)
+	}
+
+	//clear
+	os.Remove(tmp)
+	os.RemoveAll(testRoot)
+}
