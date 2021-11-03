d12ddba854afb78afd32eb2e384140ceb9984fbe
Sergey Fedyushin <SerFed13@gmail.com>
2021-11-03 08:31:54 +0600

    Add GetCLink for Bypass

--- bypass/bypass.go
+++ bypass/bypass.go
@@ -26,6 +26,10 @@ func (b *Bypass) GetURL(cLink string, options ...interface{}) (URL string) {
 	return cLink
 }
 
+func (b *Bypass) GetCLink(path string) (cLink string) {
+	return (path)
+}
+
 func (b *Bypass) Remove(cLink string) (err error) {
 	return ErrMethodNotImplemented
 }
